24ce31d8eaff556f2f3a79cfdcefddda69c3c362
unknown <lealjess2158140@gmail.com>
2022-02-21 12:32:32 +0800

    Add login page functionality

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,3 +1,5 @@
+import { Form, Field } from "react-final-form";
+import { useDispatch } from "react-redux";
 import {
   TextField,
   FormControl,
@@ -8,48 +10,88 @@ import {
   Avatar,
 } from "@mui/material";
 import { Person as PersonIcon } from "@mui/icons-material";
-
 import { paperStyles } from "./LoginPage.styles";
 
+import { login } from "../Features/userSlice";
+
+const renderTextField = ({
+  input: { name, type, onChange, value },
+  meta,
+  ...rest
+}) => (
+  <TextField
+    {...rest}
+    type={type}
+    variant="outlined"
+    margin="normal"
+    name={name}
+    helperText={meta.touched ? meta.error : undefined}
+    error={meta.error && meta.touched}
+    onChange={onChange}
+    value={value}
+  />
+);
+
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const onSubmit = (values) => {
+    dispatch(
+      login({
+        username: values.username,
+        password: values.password,
+      })
+    );
+  };
+
   return (
-    <div className="login-form">
-      <Grid>
-        <Paper elevation={3} style={paperStyles}>
-          <div className="login-title">
-            <Avatar sx={{ width: 56, height: 56 }}>
-              <PersonIcon />
-            </Avatar>
-            <Typography
-              variant="h2"
-              noWrap
-              component="div"
-              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-            >
-              Login
-            </Typography>
-          </div>
-          <FormControl className="login-inputs">
-            <TextField
-              id="username"
-              name="username"
-              label="Username"
-              variant="outlined"
-              margin="normal"
-            />
-            <TextField
-              id="password"
-              name="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              margin="normal"
-            />
-            <Button variant="contained">Submit</Button>
-          </FormControl>
-        </Paper>
-      </Grid>
-    </div>
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, submitting, values }) => (
+        <form onSubmit={handleSubmit} className="login-form">
+          <Grid>
+            <Paper elevation={3} style={paperStyles}>
+              <div className="login-title">
+                <Avatar sx={{ width: 56, height: 56 }}>
+                  <PersonIcon />
+                </Avatar>
+                <Typography
+                  variant="h2"
+                  noWrap
+                  component="div"
+                  sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+                >
+                  Login
+                </Typography>
+              </div>
+              <FormControl className="login-inputs">
+                <Field
+                  id="username"
+                  name="username"
+                  label="Username"
+                  type="text"
+                  component={renderTextField}
+                />
+                <Field
+                  id="password"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  component={renderTextField}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={submitting}
+                >
+                  Submit
+                </Button>
+              </FormControl>
+            </Paper>
+          </Grid>
+        </form>
+      )}
+    />
   );
 };
 
